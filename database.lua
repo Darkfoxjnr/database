2d65e2fdec79e919955fab5bd8c73abdaa78a5cb
DarkFoxJnr <68354543+Darkfoxjnr@users.noreply.github.com>
2022-02-24 17:42:39 +0000

    Update database.lua

--- database.lua
+++ database.lua
@@ -1,4 +1,3 @@
--- database build 1.0 beta build made to test security and reliablity of basic database for storing values and things like that
 local table = {
     ["woutt"] = "gay",
     ["darkfox"] = "gey1!",
